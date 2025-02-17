d53c7654a319e2c71ba6a4ef069559a73ffa34d9
anxapa <rapidzen16@gmail.com>
2025-02-17 15:28:54 -0800

    Updated raycasting in script
    - Made the raycast for ground-check more reliable.

--- CS4800 Project/Assets/Scripts/CharacterController.cs
+++ CS4800 Project/Assets/Scripts/CharacterController.cs
@@ -4,6 +4,7 @@ public class CharacterController : MonoBehaviour
 {
     Vector3 startPos;
     Rigidbody rb;
+    Ray groundCheck;
     public float moveSpeed;
     public float jumpForce;
     public bool isGrounded;
@@ -18,6 +19,12 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
+        // Sets the ground-check ray at the player's position.
+        Vector3 rayPos = transform.position;
+        rayPos.y += 1f;
+        groundCheck = new Ray(transform.position, Vector3.down);
+        Debug.DrawRay(rayPos, Vector3.down * 2f, Color.green);
+
         // Resets player position if player falls a certain distance.
         if (transform.position.y < 90f)
         {
@@ -29,7 +36,7 @@ public class CharacterController : MonoBehaviour
             * Time.fixedDeltaTime * moveSpeed, Space.Self);
 
         // Player jump
-        if(IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        if(isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
         }
@@ -37,9 +44,15 @@ public class CharacterController : MonoBehaviour
         // TODO: Player mouse rotation
     }
 
+    private void LateUpdate()
+    {
+        IsGrounded();
+    }
+
+    // Checks if the player is grounded using ray-casting
     bool IsGrounded()
     {
-        if (Physics.Raycast(transform.position, Vector3.down, 1f))
+        if (Physics.Raycast(groundCheck, 2f))
         {
             isGrounded = true;
             return true;
