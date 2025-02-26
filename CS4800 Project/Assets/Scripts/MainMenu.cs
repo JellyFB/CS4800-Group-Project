f68b03860e57dfb1e3fa4b4a765834f97e3abdd4
anxapa <rapidzen16@gmail.com>
2025-02-26 10:44:26 -0800

    Fixed demo
    - Added an EventSystem node in the MainMenu scene that fixed the buttons.
    - Changed the MainMenu script to properly set the scene into the level after clicking "Play".
    - Renamed the main scene "SampleScene" to "LevelScene".

--- CS4800 Project/Assets/Scripts/MainMenu.cs
+++ CS4800 Project/Assets/Scripts/MainMenu.cs
@@ -5,11 +5,11 @@ public class MainMenu : MonoBehaviour
 {
     	public void PlayGame()
     	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex * 1);
+		    SceneManager.LoadScene("LevelScene");
     	}
 
     	public void QuitGame()
     	{
-		Application.Quit();
+		    Application.Quit();
     	}
 }
