dc2a2e49eec8caf5406026609f166db1dc3d957d
anxapa <rapidzen16@gmail.com>
2025-03-26 11:46:33 -0700

    Created basic version of hotbar and task UI
    - Only set up the UI placements so far.
    - Will be adding tasks as scriptable objects.

---
+++ CS4800 Project/Assets/Scripts/Tasks/Task.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Task : ScriptableObject
+{
+    [Header("Task Properties")]
+    public string taskName;
+    public string taskDescription;
+}
---
+++ CS4800 Project/Assets/Scripts/Tasks/TaskManager.cs
@@ -0,0 +1,8 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskManager : MonoBehaviour
+{
+    [SerializeField] private List<Task> _taskList;
+}
