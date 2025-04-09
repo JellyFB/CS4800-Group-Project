46f44a3b023cd808d75fd4cc2706da0763009bdf
anxapa <rapidzen16@gmail.com>
2025-04-09 01:44:03 -0700

    Can now select between different hotbar slots

--- CS4800 Project/Assets/Scripts/Inventory/InventoryManager.cs
+++ CS4800 Project/Assets/Scripts/Inventory/InventoryManager.cs
@@ -5,21 +5,19 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _inventory;
-    [SerializeField] private int _numberOfInventorySlots = 5;
     private InventorySlot[] _slots;
+    private int _currentSlot = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _slots = GetComponentsInChildren<InventorySlot>();
 
-        if (_slots.Length != _numberOfInventorySlots)
-            Debug.LogError("Number of inventory slots is wrong!");
 
         // Makes sure the amount of items within the inventory is less than or equal to the max.
-        while (_inventory.Count > _numberOfInventorySlots)
+        while (_inventory.Count > _slots.Length)
         {
-            _inventory.RemoveAt(_numberOfInventorySlots);
+            _inventory.RemoveAt(_slots.Length);
         }
 
         // Assigns the icons to each of the hotbar.
@@ -37,7 +35,7 @@ public class InventoryManager : MonoBehaviour
     // Checks for numeric input for hotbar switching.
     private void CheckInput()
     {
-        for(int i = 1; i <= _numberOfInventorySlots; i++)
+        for(int i = 1; i <= _slots.Length; i++)
         {
             if(Input.GetKeyDown(KeyCode.Alpha0 + i))
             {
@@ -50,16 +48,20 @@ public class InventoryManager : MonoBehaviour
     // Should switch the model the player is holding and should highlight the hotbar slot the player is on.
     private void SwitchSlot(int slotNumber)
     {
-        
+        _slots[_currentSlot - 1].Deselect();
+
+        _currentSlot = slotNumber;
+
+        _slots[_currentSlot - 1].Select();
     }
 
     // On pickup, adds the item to the hotbar.
     // Will be called by another method outside of class.
     private bool PickupItem(GameObject item)
     {
-        for (int i = 0; i < _numberOfInventorySlots; i++)
+        for (int i = 0; i < _slots.Length; i++)
         {
-            if (i >= _numberOfInventorySlots)
+            if (i >= _slots.Length)
             {
                 _inventory.Add(item);
             }
--- CS4800 Project/Assets/Scripts/Inventory/InventorySlot.cs
+++ CS4800 Project/Assets/Scripts/Inventory/InventorySlot.cs
@@ -1,20 +1,39 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 // A gameobject that handles an inventory slot of an inventory.
 public class InventorySlot : MonoBehaviour
 {
+    private Image _image;
+
+    // Selection alpha colors
+    private const float _COLOR_ALPHA_SELECTED = 1f;
+    private const float _COLOR_ALPHA_DESELECTED = 0.40f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         if (GetComponentInParent<InventoryManager>() == null)
             Debug.LogError($"{this} is not under a parent InventoryManager!");
 
-
+        _image = GetComponent<Image>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Select()
     {
-        
+        ChangeImageAlpha(_COLOR_ALPHA_SELECTED);
+    }
+
+    public void Deselect()
+    {
+        ChangeImageAlpha(_COLOR_ALPHA_DESELECTED);
+    }
+
+    private void ChangeImageAlpha(float newAlpha)
+    {
+        Color newColor = _image.color;
+        newColor.a = newAlpha;
+
+        _image.color = newColor;
     }
 }
