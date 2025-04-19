015f91de74c1a4eac2d7bc52db8d1f1b85481b70
Mario Hermiz <134554555+mhermiz@users.noreply.github.com>
2025-04-18 20:49:54 -0700

    Added mouse sensitivity to settings
    - Added adjustable mouse sensitivity

--- CS4800 Project/Assets/Scripts/Player/PlayerCam.cs
+++ CS4800 Project/Assets/Scripts/Player/PlayerCam.cs
@@ -1,11 +1,12 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCam : MonoBehaviour
 {
 	// Mouse sensitivity
-	public float sensX;
-	public float sensY;
+	public float sensX = 180f;
+	public float sensY = 180f;
 
 	// Player
 	public Transform player;
@@ -14,27 +15,41 @@ public class PlayerCam : MonoBehaviour
 	float _xRotation;
 	float _yRotation;
 
-		// Start is called before first frame update
+	// Slider bar for changing sensitivity
+	public Slider slider;
+
+	// Start is called before first frame update
     	private void Start()
     	{
-			Cursor.lockState = CursorLockMode.Locked;
-			Cursor.visible = false;
-		}
+		sensX = PlayerPrefs.GetFloat("currentSensitivty", 180);
+		sensY = PlayerPrefs.GetFloat("currentSensitivty", 180);
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		slider.value = sensX/2;
+	}
 
-		// Update is called once per frame
+	// Update is called once per frame
     	private void Update()
-    	{
-			// get mouse input
-			float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-			float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+    	{	
+		PlayerPrefs.SetFloat("currentSensitivity", sensX);
+		PlayerPrefs.SetFloat("currentSensitivity", sensY);
 
-			_yRotation += mouseX;
-			_xRotation -= mouseY;
+		// get mouse input
+		float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+		float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
-			_xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+		_yRotation += mouseX;
+		_xRotation -= mouseY;
 
-			// rotate cam and orientation
-			transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
-			player.rotation = Quaternion.Euler(0, _yRotation, 0);
+		_xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+
+		// rotate cam and orientation
+		transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
+		player.rotation = Quaternion.Euler(0, _yRotation, 0);
     	}
+
+	public void AdjustSpeed(float newSpeed) {
+		sensX = newSpeed * 5;
+		sensY = newSpeed * 5;
+	}
 }
