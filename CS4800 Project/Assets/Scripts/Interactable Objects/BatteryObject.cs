423eac745209eb959f74f363ae99f68c293bd3ca
JellyFB <131621190+JellyFB@users.noreply.github.com>
2025-04-20 18:01:10 -0700

    made some changes to the level and added tools

--- CS4800 Project/Assets/Scripts/Interactable Objects/BatteryObject.cs
+++ CS4800 Project/Assets/Scripts/Interactable Objects/BatteryObject.cs
@@ -1,10 +1,18 @@
+using System;
+using System.Runtime.CompilerServices;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class BatteryObject : Interactable
 {
+    private String[] status = {"high", "low"};
+    private String temp;
+    private bool voltage;
+    // Identifier for the specific object in game 
     private void Start()
     {
         objectName = "Battery";
+        temp = status[UnityEngine.Random.Range(0,2)];
     }
 
     public override void Interact()
@@ -12,7 +20,7 @@ public class BatteryObject : Interactable
         // Get player currently in hand/pocket
         Item item = PlayerManager.instance.inventoryManager.GetCurrentHeldItem();
 
-        // Only interact if the item is a shovel
+        // Interactions for the object - not implemented yet
         if (item != null && item.itemName.Equals("Null"))
         {
             Destroy(gameObject);
