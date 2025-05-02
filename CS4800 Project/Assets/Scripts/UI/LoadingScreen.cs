dc24fb15e173363c52134dc752be943673f682e1
indigopaku <108289555+indigopaku@users.noreply.github.com>
2025-05-02 10:36:39 -0700

    Add files via upload

---
+++ CS4800 Project/Assets/Scripts/UI/LoadingScreen.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public Slider progressBar;
+    public TextMeshProUGUI progressText;
+
+    void Start()
+    {
+        StartCoroutine(LoadSceneAsync("MainGameScene"));
+    }
+
+    IEnumerator LoadSceneAsync(string sceneName)
+{
+    AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+    operation.allowSceneActivation = false; // Don't switch immediately
+
+    float elapsedTime = 0f;
+    float totalTime = 10f; // Simulated 10 seconds
+
+    while (elapsedTime < totalTime)
+    {
+        elapsedTime += Time.deltaTime;
+        float progress = Mathf.Clamp01(elapsedTime / totalTime);
+        progressBar.value = progress;
+        progressText.text = (progress * 100f).ToString("F0") + "%";
+        yield return null;
+    }
+
+    // Once fake loading is done, activate the scene
+    operation.allowSceneActivation = true;
+}
+
+}
